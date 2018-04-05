79efbd7f77400f941f564c45d9e287962dd848fa
“calvin” <15156855792@163.com>
2018-04-05 23:11:11 +0800

    修改启动文件java

--- sell/src/main/java/com/calvin/sell/SellApplication.java
+++ sell/src/main/java/com/calvin/sell/SellApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SellApplication {
 
 	public static void main(String[] args) {
-        System.out.println("====44444======");
 	    SpringApplication.run(SellApplication.class, args);
 	}
 }
